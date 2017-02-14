1be96f2f7c4b59597bef60eb8322c2566575872e
rahulkulkarnicapgemini <rahul.b.kulkarni@capgemini.com>
2017-02-14 14:36:00 +0530

    Add files via upload

--- services/service.php
+++ services/service.php
@@ -1,15 +1,24 @@
+
+
 <?php 
 
 //Header for access
- header('Access-Control-Allow-Origin: *');  
+ header('Access-Control-Allow-Origin: *');
  
-$BaseURL = "https://c86b4112-3874-4da8-9850-088c89ea35fc-bluemix.cloudant.com";
+$BaseURL = "https://a6a59714-f2ea-4b8d-bc41-e15f94fe5252-bluemix.cloudant.com"; //Database subscription.
+
+
+
+$DefaultDB = "iotp_if34ew_compressors_2017-02";//Please implement logic to change this to current month.
+
+
+
   
  if($_GET["URL"]==null)
  {
 
 	//Default URL 
-	$url = $BaseURL."/asc";
+	$url = $BaseURL."/".$DefaultDB; //Default Database
 	
  }
  else
@@ -18,20 +27,15 @@ $BaseURL = "https://c86b4112-3874-4da8-9850-088c89ea35fc-bluemix.cloudant.com";
  	
  }
  
+
   
- if($_GET["ALLDBS"]=="TRUE")
+ if($_GET["ALLDOCS"]=="TRUE")
  {
- 	$url =  $BaseURL."/_all_dbs"; 
+ 	$url =  $url ."/_all_docs"; 
  	
  }
- 
 
 //Set up call to the service
-
-
 $result = file_get_contents($url); 
 echo($result);
-
-
-
 ?>
