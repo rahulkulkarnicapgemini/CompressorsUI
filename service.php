e33393d6fea4b54bcde5754d99ec90c95f1fe643
rahulkulkarnicapgemini <rahul.b.kulkarni@capgemini.com>
2017-02-17 13:49:54 +0530

    Update service.php

--- service.php
+++ service.php
@@ -1,17 +1,11 @@
 <?php
+  //parse vcap
+  if( getenv("VCAP_SERVICES") ) {
+      $json = getenv("VCAP_SERVICES");
+  } 
+  # No DB credentials
+  else {
+      throw new Exception("No Database Information Available.", 1);
+  }
 
-if(getenv( "VCAP_SERVICES"))
-{    
-  /*
-  $details  = json_decode( getenv( "VCAP_SERVICES" ), true );   
-   $dsn      = $details [ "dashDB" ][0][ "credentials" ][ "dsn" ];    
-  $ssl_dsn  = $details [ "dashDB" ][0][ "credentials" ][ "ssldsn" ];       
-  $driver = "DRIVER={IBM DB2 ODBC DRIVER};";   
-  $conn_string = $driver . $dsn; 
-  echo($conn_string); */
-}
-else
-{
-  echo("No Credentials");
-}
 ?>
