0cc88e3b54ea604af6b92577197e047d687bb21f
rahulkulkarnicapgemini <rahul.b.kulkarni@capgemini.com>
2017-02-17 13:37:04 +0530

    Update service.php

--- service.php
+++ service.php
@@ -1,5 +1,16 @@
 <?php
 
-echo("hELLO wORLD");
-
+if( getenv( "VCAP_SERVICES" ))
+{    
+  $details  = json_decode( getenv( "VCAP_SERVICES" ), true );   
+  $dsn      = $details [ "dashDB" ][0][ "credentials" ][ "dsn" ];    
+  $ssl_dsn  = $details [ "dashDB" ][0][ "credentials" ][ "ssldsn" ];       
+  $driver = "DRIVER={IBM DB2 ODBC DRIVER};";   
+  $conn_string = $driver . $dsn; 
+  echo($conn_string);
+}
+else
+{
+  echo("No Credentials");
+}
 ?>
